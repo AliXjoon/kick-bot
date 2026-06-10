bce54fa85a9976a9f8e572dc25cb63082f77e70c
AliXjoon <aligheisari1400@gmail.com>
2026-06-10 05:39:51 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,54 @@
-import time
+import asyncio
+import requests
 from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup
 
 TOKEN = "8902801768:AAHW3qPXi_d6f8ozWAy_13TfYX9GWIfuBMw"
 CHANNEL = "@am1razzzz"
+USERNAME = "am1razzz"
 
 bot = Bot(token=TOKEN)
 
-def send_live():
+def is_live():
+    try:
+        url = f"https://kick.com/api/v1/channels/{USERNAME}"
+        r = requests.get(url, timeout=10)
+        data = r.json()
+        return data.get("livestream") is not None
+    except:
+        return False
+
+async def send_live():
     keyboard = [
-        [InlineKeyboardButton("🟢 Live 🟢", url="https://kick.com/am1razzzz")]
+        [InlineKeyboardButton("🟢Live🟢", url=f"https://kick.com/{USERNAME}")]
     ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
 
+    caption = "<b>🔴 LIVE STARTED</b>\n\nStream is now live 👇"
+
     with open("am1razzzz.png", "rb") as photo:
-        bot.send_photo(
+        await bot.send_photo(
             chat_id=CHANNEL,
             photo=photo,
-            caption="<b>🔴 LIVE STREAM STARTED</b>",
+            caption=caption,
             parse_mode="HTML",
             reply_markup=reply_markup
         )
 
-print("Bot started")
+async def main():
+    print("Bot started")
 
-send_live()
+    was_live = False
 
-# 🔥 این قسمت باعث میشه Railway نزنه ببنده
-while True:
-    time.sleep(60)
+    while True:
+        live = is_live()
+
+        if live and not was_live:
+            await send_live()
+
+        was_live = live
+
+        await asyncio.sleep(60)
+
+if __name__ == "__main__":
+    asyncio.run(main())
